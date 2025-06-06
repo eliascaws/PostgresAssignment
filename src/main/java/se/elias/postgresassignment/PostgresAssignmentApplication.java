40a6eb09859704be8e2e02436f6b56e61fddb2dc
eliascaws <eliasfagerstrom@gmail.com>
2025-06-06 13:36:11 +0200

    Initial commit

---
+++ src/main/java/se/elias/postgresassignment/PostgresAssignmentApplication.java
@@ -0,0 +1,13 @@
+package se.elias.postgresassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresAssignmentApplication.class, args);
+    }
+
+}
