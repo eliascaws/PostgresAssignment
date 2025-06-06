26b33ea792565b4b5febcaa2a3155bf7deb04cd2
eliascaws <eliasfagerstrom@gmail.com>
2025-06-06 18:00:58 +0200

    feat: add controller and html
    Introduce HomeController and associated HTML pages.
    
    fix added getters and setters to Match and Team entity classes

---
+++ src/main/java/se/elias/postgresassignment/controllers/HomeController.java
@@ -0,0 +1,71 @@
+package se.elias.postgresassignment.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import se.elias.postgresassignment.model.Match;
+import se.elias.postgresassignment.model.MatchRepository;
+import se.elias.postgresassignment.model.Team;
+import se.elias.postgresassignment.model.TeamRepository;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    private final MatchRepository matchRepository;
+    private final TeamRepository teamRepository;
+
+    @Autowired
+    public HomeController(MatchRepository matchRepository, TeamRepository teamRepository) {
+        this.matchRepository = matchRepository;
+        this.teamRepository = teamRepository;
+    }
+
+    @GetMapping("/")
+    public String schedulePage(Model model) {
+        List<Match> matches = (List<Match>) matchRepository.findAll();
+        model.addAttribute("matches", matches);
+        return "home";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage(Model model) {
+        model.addAttribute("team", new Team());
+        model.addAttribute("match", new Match());
+        model.addAttribute("teams", teamRepository.findAll());
+        return "admin";
+    }
+
+    @PostMapping("/admin/add-game")
+    public String addTeam(@ModelAttribute("team") Team team) {
+        teamRepository.save(team);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/add-match")
+    public String addMatch(@ModelAttribute("match") Match match, Model model) {
+        if (match.getTeam1().getId().equals(match.getTeam2().getId())){
+            model.addAttribute("match", match);
+            model.addAttribute("team", new Team());
+            model.addAttribute("tems", teamRepository.findAll());
+            model.addAttribute("error", "Team 1 and 2 must be different");
+            return "admin";
+        }
+
+        matchRepository.save(match);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+        return "login";
+    }
+
+
+
+}
--- src/main/java/se/elias/postgresassignment/model/Match.java
+++ src/main/java/se/elias/postgresassignment/model/Match.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 public class Match {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @ManyToOne
     private Team team1;
@@ -22,4 +22,43 @@ public class Match {
     @Enumerated(EnumType.STRING)
     private Region region;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Team getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(Team team1) {
+        this.team1 = team1;
+    }
+
+    public Team getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(Team team2) {
+        this.team2 = team2;
+    }
+
+    public LocalDateTime getScheduledTime() {
+        return scheduledTime;
+    }
+
+    public void setScheduledTime(LocalDateTime scheduledTime) {
+        this.scheduledTime = scheduledTime;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
 }
